a0bcec78beff2f49d96fbe2fb17c43b63a7b260d
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-09 16:37:07 +0100

    Add fourth failing test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,7 +9,11 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
-  it 'returs "fizzbuzz" when passed 15' do
+  it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns the number if not multiple of 3 or 5' do
+    expect(fizzbuzz(4)).to eq '4'
   end
 end
